82ee180cd1c290cbf9b627dbaabf9793b81a9fa0
MARVIN <o.silverygold@gmail.com>
2024-09-27 11:16:15 +0300

    Add counter component

---
+++ src/components/Counter.tsx
@@ -0,0 +1,21 @@
+import useCounterStore from "../store/CounterStore";
+
+const Counter = () => {
+  const { count, increment, decrement, reset } = useCounterStore();
+  return (
+    <div>
+      <p>{count}</p>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+      <button type="button" onClick={decrement}>
+        Decrement
+      </button>
+      <button type="button" onClick={reset}>
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
